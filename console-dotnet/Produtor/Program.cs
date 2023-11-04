53e57154baa18590be0adadbb141cc3cf3fe3246
Wesley Alves <wesley.info07@gmail.com>
2023-11-04 16:03:00 -0300

    Criando um produtor

--- console-dotnet/Produtor/Program.cs
+++ console-dotnet/Produtor/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092"};
+
+using var producer = new ProducerBuilder<string, string>(config).Build();
+
+var message = new Message<string, string>
+{
+    Key = Guid.NewGuid().ToString(),
+    Value = $"Mensagem teste {Guid.NewGuid().ToString()}"
+};
+
+var result = await producer.ProduceAsync("topico-teste", message);
+
+Console.WriteLine($"Partição: ${result.Partition} OffSet: {result.Offset}");
