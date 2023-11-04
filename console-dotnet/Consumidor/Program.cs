7b2c78b936fca2eb39d517ef882f3b7d327be2e6
Wesley Alves <wesley.info07@gmail.com>
2023-11-04 16:07:09 -0300

    Criando um consumidor

--- console-dotnet/Consumidor/Program.cs
+++ console-dotnet/Consumidor/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    GroupId = "devio",
+    BootstrapServers = "localhost:9092" //onde meu broker está rodando
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+consumer.Subscribe("topico-teste");
+
+while (true)
+{
+    var result = consumer.Consume();
+    Console.WriteLine($"Mensagem: {result.Message.Key} - {result.Message.Value}");
+}
