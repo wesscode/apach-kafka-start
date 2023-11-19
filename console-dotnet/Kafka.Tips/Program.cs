9ee1620002af563adb947fd72ec5db483695f7db
Wesley Alves <wesley.info07@gmail.com>
2023-11-18 22:48:25 -0300

    Aplicando Headers e Tracing

--- console-dotnet/Kafka.Tips/Program.cs
+++ console-dotnet/Kafka.Tips/Program.cs
@@ -1,7 +1,117 @@
+using System.Text;
 using Confluent.Kafka;
 
 #region Aplicando Headers e Tracing
+const string Topico = "desenvolvedor.io";
+var i = 1;
 
+_ = Task.Run(() => Consumir("grupo4", AutoOffsetReset.Latest));
+
+while (true)
+{
+    Console.ReadLine();
+    Produzir(i).GetAwaiter().GetResult();
+    i++;
+}
+
+static async Task Produzir(int i)
+{
+    var configuracao = new ProducerConfig
+    {
+        //Habilitando idempotêcia
+        BootstrapServers = "localhost:9092",
+        Acks = Acks.All, //envia replica da mensagem para todos os novos replication factors
+        MaxInFlight = 1, //determina a quantidade de conexões na sessão estabelecida
+        MessageSendMaxRetries = 2, //Garante que se houver alguma instabilidade, a mensagem tentará ser enviada 2x(opcional para idempotência)
+
+        TransactionalId = Guid.NewGuid().ToString() //Necessário para trabalhar com transações
+    };
+    var key = Guid.NewGuid().ToString();
+    var mensagem = $"Mensagem ( {i} ) KEY: {key}";
+
+    Console.WriteLine(">> Enviada:\t " + mensagem);
+
+    try
+    {
+        using var producer = new ProducerBuilder<string, string>(configuracao).Build();
+
+        //Iniciar uma transação
+        producer.InitTransactions(TimeSpan.FromSeconds(5));
+        producer.BeginTransaction();
+
+
+        //Aplicando Headers na minha aplicação.
+        var headers = new Headers();
+        headers.Add("application", Encoding.UTF8.GetBytes("payment")); //identificando que é um app de pagamento.
+        headers.Add("transactionId", Encoding.UTF8.GetBytes(Guid.NewGuid().ToString()));
+
+        //Envia mensagem 1
+        var result = await producer.ProduceAsync(Topico, new Message<string, string>
+        {
+            Key = key,
+            Value = mensagem,
+            Headers = headers
+        });
+
+        //Envia mensagem 2
+        //Envia mensagem 3
+        //Atualiza o banco
+
+        //confirma a transação
+        producer.CommitTransaction();
+
+        //aborta a transação
+        producer.AbortTransaction();
+
+        await Task.CompletedTask;
+    }
+    catch (Exception ex)
+    {
+        Console.Error.WriteLine(ex.ToString());
+    }
+}
+
+static void Consumir(string grupoId, AutoOffsetReset autoOffsetReset)
+{
+    var clientId = Guid.NewGuid().ToString()[0..5];
+
+    var conf = new ConsumerConfig
+    {
+        ClientId = clientId,
+        GroupId = grupoId,
+        BootstrapServers = "localhost:9092",
+        AutoOffsetReset = autoOffsetReset,
+        EnablePartitionEof = true,
+        EnableAutoCommit = false,
+
+        // Configuração para consumir somente mensagens confirmadas.
+        IsolationLevel = IsolationLevel.ReadUncommitted
+    };
+
+    using var consumer = new ConsumerBuilder<string, string>(conf).Build();
+    consumer.Subscribe(Topico);
+
+    while (true)
+    {
+        var result = consumer.Consume();
+
+        if (result.IsPartitionEOF)
+            continue;
+
+        //convertendo os headers que chegou na aplicação para um dictionary.
+        var headers = result
+            .Message
+            .Headers
+            .ToDictionary(p => p.Key, p => Encoding.UTF8.GetString(p.GetValueBytes()));
+
+            var application = headers["application"];
+            var transactionId = headers["transactionId"];
+
+        var message = "<< Recebida: \t" + result.Message.Value;
+        Console.WriteLine(message);
+        consumer.Commit(result);
+    }
+}
 
 #endregion
 
@@ -47,6 +157,7 @@ static async Task Produzir(int i)
         //Envia mensagem 1
         var result = await producer.ProduceAsync(Topico, new Message<string, string>
         {
+            Key = key,
             Value = mensagem
         });
 
